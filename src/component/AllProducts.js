25f24661775b0f40edbf259f71d46b2a75f5795a
Ai Shadhin <aishadhin.ac@gmail.com>
2022-07-20 17:16:25 +0600

    grid header added

--- src/component/AllProducts.js
+++ src/component/AllProducts.js
@@ -4,16 +4,20 @@ import SingleProduct from './SingleProduct';
 const AllProducts = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-                fetch('books.json')
-                .then(res => res.json())
-                .then(data => setProducts(data))
-              } , []);
-              console.log(products)
+        fetch('books.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+    console.log(products)
     return (
-        <div className='grid grid-cols-3 container mx-auto my-16 gap-5'>
-            {
-                products.slice(0,6).map(book =><SingleProduct book={book}></SingleProduct>)
-            }
+        <div>
+            <h2 className='uppercase text-primary text-lg mt-16 font-bold'>Popular book</h2>
+            <div className='h-[3px] w-[120px] bg-primary mt-2 mx-auto'></div>
+            <div className='grid grid-cols-3 container mx-auto my-16 gap-5'>
+                {
+                    products.slice(0, 6).map(book => <SingleProduct book={book}></SingleProduct>)
+                }
+            </div>
         </div>
     );
 };
