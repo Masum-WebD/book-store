c3eba500d03f6b6fb61421370fdc548836b8777e
mdrokib231185 <mdrokhasan231185@gmail.com>
2022-07-21 18:24:32 +0600

    somthing change

--- src/component/AllProducts.js
+++ src/component/AllProducts.js
@@ -11,7 +11,7 @@ const AllProducts = () => {
     console.log(products)
     return (
         <div>
-            <h2 className='uppercase text-primary text-lg mt-16 font-bold'>Popular books</h2>
+            <h2 className='uppercase text-primary text-lg mt-20 font-bold'>Popular books</h2>
             <div className='h-[3px] w-[120px] bg-primary mt-2 mx-auto'></div>
             <div className='md:grid grid-cols-3 px-4 md:container mx-auto my-16 gap-5'>
                 {
