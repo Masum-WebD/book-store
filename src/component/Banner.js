75ad729d47e9be7d6a0365b424504e9cf0537a22
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-07-24 17:31:08 +0600

    Minor changes in ui

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { BsSearch } from "react-icons/bs";
-import image1 from "../../src/asset/Images/shiromani-kant-mo3FOTG62ao-unsplash.jpg";
 import "./Banner.css";
 
 const Banner = () => {
   return (
     <div class="hero ">
-      <img className="banner" />
+      <img className="banner" alt="" />
       <div></div>
       <div class="hero-content text-center text-neutral-content">
         <div class="max-w-md">
@@ -14,16 +13,16 @@ const Banner = () => {
             THE EASIEST WAY TO BUY ANY BOOK
           </h1>
 
-          <div className="background flex ">
+          <div className="background flex shadow-2xl">
             <div>
               <input
                 type="text"
-                placeholder="Type here"
-                class="input mt-3   w-full max-w-xs"
+                placeholder="Search your book"
+                class="input mt-3 ml-2 w-full max-w-xs"
               />
             </div>
             <div className="mt-3">
-              <button class="btn btn-color mx-4">
+              <button class="btn btn-primary text-white mx-4">
                 SEARCH
                 <span className="mx-2">
                   <BsSearch />
--- src/share/Footer/Footer.js
+++ src/share/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { FaCcPaypal, FaCcVisa } from "react-icons/fa";
-import { SiAmericanexpress, SiMastercard } from "react-icons/si";
 import ScrollToTop from "react-scroll-to-top";
 import "./Footer.css";
 import ItemsContainer from "./ItemsContainer";
+import paypal from "../../asset/Icons/paypal.png";
+import visa from "../../asset/Icons/visa.png";
+import masterCard from "../../asset/Icons/mastercard-26128.png";
+import americanExpress from "../../asset/Icons/american-express.png";
 
 const Footer = () => {
   return (
@@ -11,16 +13,19 @@ const Footer = () => {
       <ItemsContainer />
       <div
         className="grid under-footer grid-cols-1 sm:grid-cols-2 lg:grid-cols-3
-      my-auto pt-2 text-black items-center text-sm pb-8"
+      my-auto pt-6 text-black items-center text-sm pb-8"
       >
         <span>© 2022 Book Store. All rights reserved.</span>
         <span>Terms · Privacy Policy</span>
-        <span className="text-3xl text-gray-500 flex gap-3">
-          <FaCcPaypal /> <FaCcVisa /> <SiMastercard /> <SiAmericanexpress top='1000' />
-        </span>
+        <div className="flex mx-auto">
+          <img className="w-14 mx-2" src={paypal} alt="" />
+          <img className="w-14 mx-2" src={visa} alt="" />
+          <img className="w-14 mx-2" src={masterCard} alt="" />
+          <img className="w-14 mx-2" src={americanExpress} alt="" />
+        </div>
       </div>
       <ScrollToTop smooth />
-      
+
     </footer>
   );
 };
