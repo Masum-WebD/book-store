ddeae880e7ceada8f46d3757cde477b1b8b161cb
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-07-25 15:09:29 +0600

    Added a modal for logout

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,8 +3,42 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../asset/Images/logo.png";
+import { FiLogIn } from 'react-icons/fi';
+import { FiLogOut } from 'react-icons/fi';
+import auth from "../Firebase/firebase.init";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
+import { Modal } from "@mui/material";
+import Box from '@mui/material/Box';
+import { toast } from 'react-toastify';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '5px solid #229955',
+  boxShadow: 24,
+  p: 4,
+  borderRadius: "10px"
+};
 
 const Header = () => {
+
+  const [user] = useAuthState(auth);
+
+  const logout = () => {
+    signOut(auth);
+    handleClose();
+    toast.success("Logout successful")
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div class="navbar bg-primary fixed top-0 z-50">
       <div class="navbar-start lg:mx-5">
@@ -73,10 +107,34 @@ const Header = () => {
         <Link to="">
           <FontAwesomeIcon className="mr-5 h-6" icon={faCartShopping} />
         </Link>
-        <div class="">
-          <Link to='/login' class="btn text-white">Log in</Link>
-        </div>
+        {
+          user ?
+
+            <div class="">
+              <button onClick={handleOpen} class="btn text-white">Log Out <FiLogOut className="text-xl ml-2" /></button>
+            </div>
+
+            :
+
+            <div class="">
+              <Link to='/login' class="btn text-white">Log in <FiLogIn className="text-xl ml-2" /></Link>
+            </div>
+        }
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <h1 className="text-black text-2xl text-center font-semibold">Are sure for logout?</h1>
+          <div className="mt-6 flex justify-evenly">
+            <button onClick={logout} className="btn">Yes</button>
+            <button onClick={handleClose} className="btn">Cancel</button>
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
 };
