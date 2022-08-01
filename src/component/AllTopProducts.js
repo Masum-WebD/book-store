b91f1b02c576799df11cda59485c67b993817502
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-08-01 14:44:22 +0600

    Added a delivery section in home page

--- src/component/AllTopProducts.js
+++ src/component/AllTopProducts.js
@@ -15,7 +15,7 @@ const AllTopProducts = () => {
         Top Selling books
       </h2>
       <div className="h-[3px] w-[120px] bg-primary mt-2 mx-auto"></div>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 px-4 md:container mx-auto my-16 gap-5">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 px-4 md:container mx-auto mt-16 lg:mb-0 mb-16 gap-5">
         {products.slice(9, 12).map((book) => (
           <AllProductSingle book={book}></AllProductSingle>
         ))}
