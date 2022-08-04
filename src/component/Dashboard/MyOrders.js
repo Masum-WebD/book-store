ea6a6b415816f037de919771ccd4a574d33f5c27
Alneed Roy <alneedrs2@gmail.com>
2022-08-04 17:47:44 +0600

    added dashboard design

--- src/component/Dashboard/MyOrders.js
+++ src/component/Dashboard/MyOrders.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const MyOrders = () => {
-      return (
-            <div>
-                  <h1 className='text-3xl text-black'>This is Order page</h1>
-            </div>
-      );
+  return (
+    <div className="grid lg:grid-cols-2 border-2 border-gray-400 rounded-md mx-10">
+      <div className="my-5">
+        <h2 className="text-2xl text-gray-700">My Orders</h2>
+        <h3 className="text-xl text-gray-500">Your Total Order : 0</h3>
+      </div>
+      <div className="flex justify-center items-center gap-3 my-5 lg:my-0">
+        <div>
+          <h2 className="text-xl text-gray-500">Status : </h2>
+        </div>
+        <div>
+          <select class="select select-accent text-gray-500 w-full max-w-xs">
+            <option disabled selected>
+              Select Any
+            </option>
+            <option>Processing</option>
+            <option>Shipped</option>
+            <option>Completed</option>
+            <option>Cancelled</option>
+            <option>Returned</option>
+          </select>
+        </div>
+        <div>
+          <button class="btn btn-primary text-white">Submit</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyOrders;
