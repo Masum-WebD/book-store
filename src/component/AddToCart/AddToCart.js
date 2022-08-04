45fcfe77e70321818e8925ec984f39bb28b4a527
Ai Shadhin <aishadhin.ac@gmail.com>
2022-08-04 15:17:26 +0600

    cart

--- src/component/AddToCart/AddToCart.js
+++ src/component/AddToCart/AddToCart.js
@@ -14,12 +14,12 @@ const AddToCart = () => {
 
   return (
     <div className="bg-[#F9FAFB]">
-      <div className="pt-32 pb-16 flex gap-10 text-left text-neutral container mx-auto">
-        <div className=" w-1/2">
+      <div className="pt-32 pb-16 flex flex-col-reverse lg:flex-row gap-10 text-left text-neutral container mx-auto">
+        <div className=" lg:w-1/2 px-6 lg:px-0">
           <ShopUserInfo />
         </div>
-        <div className=" w-1/2">
-          <h2 className="text-2xl text-left text-neutral font-medium mb-4">Order Summary</h2>
+        <div className=" lg:w-1/2 px-6 lg:px-0">
+          <h2 className="text-2xl text-left text-neutral font-medium mt-5 lg:mt-0 mb-4">Order Summary</h2>
           <div className=" bg-white">
             {cartProduct.map((p) => (
               <CartProduct key={p._id} product={p}></CartProduct>
