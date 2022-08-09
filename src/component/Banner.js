0659d63f723277e795e40770d67f59a88eda03ce
Ai Shadhin <aishadhin.ac@gmail.com>
2022-08-09 12:32:22 +0600

    h

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,8 +1,24 @@
-import React from "react";
-import { FaSearch } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
 import "./Banner.css";
 
 const Banner = () => {
+
+  const [searchText, setSearchText] = useState('');
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const url = `http://localhost:5000/product?name=${searchText}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setBooks(data))
+  }, [searchText])
+
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setSearchText(e.target.name.value)
+  }
+
   return (
     <section className="">
       <div class="banner">
@@ -10,7 +26,7 @@ const Banner = () => {
           <div class="text-center text-neutral-content">
             <div class="">
               <h1 class="mb-5 text-5xl font-bold leading-[60px] text-white">
-                THE EASIEST WAY TO<br/> BUY ANY BOOK
+                THE EASIEST WAY TO<br /> BUY ANY BOOK
               </h1>
             </div>
           </div>
@@ -19,8 +35,16 @@ const Banner = () => {
 
       <div className="lg:w-3/5 w-4/5 lg:p-3 p-2 mx-auto lg:-mt-9 -mt-9 relative bg-white rounded-lg shadow-lg">
         <div class="relative">
-          <input type="text" placeholder="Search your book" class="input input-bordered rounded-md input-primary text-black w-full " />
-          <button class="btn btn-primary absolute top-0 right-0 rounded-l-none text-white">Search <FaSearch className="ml-2" /></button>
+          <form onSubmit={handleSubmit}>
+            <input type="text" name="name" placeholder="Search by book name" class="input input-bordered rounded-md input-primary text-black w-full " />
+            <input type="submit" value="Search" class="btn btn-primary absolute top-0 right-0 rounded-l-none text-white" />
+          </form>
+          <div>
+            <h3>
+              {books.length}
+            </h3>
+          </div>
+
         </div>
       </div>
     </section >
