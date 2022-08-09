47d5083ba8e22cc43e725ed31df622d1f7db302e
Alneed Roy <alneedrs2@gmail.com>
2022-08-09 16:13:59 +0600

    add

--- src/component/Dashboard/MyOrders.js
+++ src/component/Dashboard/MyOrders.js
@@ -7,7 +7,7 @@ const MyOrders = () => {
       <div className="grid lg:grid-cols-2 border-2 border-t-4 border-gray-400 border-t-green-400 rounded-md lg:mx-48">
         <div className="my-5">
           <h2 className="text-2xl font-bold text-gray-700">My Orders</h2>
-          <h3 className="text-xl text-gray-500">Your Total Order : 1</h3>
+          <h3 className="text-xl text-gray-500">Your Total Order : 2</h3>
         </div>
         <div className="flex justify-center items-center gap-3 my-5 lg:my-0">
           <div>
