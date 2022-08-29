ff0c16be0de8ec885801f1e16401077ed7d352b7
Ai Shadhin <aishadhin.ac@gmail.com>
2022-08-29 23:44:48 +0600

    design

--- src/component/AllTopProducts.js
+++ src/component/AllTopProducts.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import AllProductSingle from "./AllProductSingle";
 import SingleProduct from "./SingleProduct";
 
 const AllTopProducts = () => {
--- src/component/Header.js
+++ src/component/Header.js
@@ -136,7 +136,7 @@ const Header = () => {
                 <div class="indicator mr-2 h-6">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="h-8 w-8"
+                    class="h-5 w-5"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -148,7 +148,7 @@ const Header = () => {
                       d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                     />
                   </svg>
-                  <span class="badge badge-primary badge-sm indicator-item">{cartProduct.length} </span>
+                  <span class="badge text-[10px] badge-primary badge-sm indicator-item">{cartProduct.length} </span>
                 </div>
               </label>
             </NavLink>
