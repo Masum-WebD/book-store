e789b809f09e3d5fa34d12341947ec49d9397c04
Ai Shadhin <aishadhin.ac@gmail.com>
2022-08-29 15:16:37 +0600

    home

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -13,7 +13,7 @@ const Banner = () => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    const url = `https://the-online-book-shop.herokuapp.com/product/?name=${searchText}`;
+    const url = `http://localhost:5000/product/?name=${searchText}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => setBooks(data));
@@ -27,40 +27,38 @@ const Banner = () => {
   return (
     <section className="relative bg-[#F5FFF7]">
       <div class="bg-no-repeat bg-cover" style={{ backgroundImage: `url(${bg})` }}>
-        <div className="lg:py-[90px] max-w-[1196px] mx-auto">
-          <div class="text-left text-neutral-content w-1/2">
+        <div className="lg:py-[90px] py-[50px] px-4 max-w-[1196px] mx-auto">
+          <div class="text-left text-neutral-content lg:w-1/2">
             <div class="">
-              <h3 className="text-neutral text-[16px] font-medium capitalize">
+              <h3 className="text-neutral text-sm lg:text-[16px] font-medium capitalize">
                 Explore our online shop and
               </h3>
-              <h1 class="mb-5 lg:text-[64px] mt-2 text-4xl font-bold lg:leading-[81px] text-[#0B1B28] capitalize ">
+              <h1 class="mb-5 lg:text-[64px] w-2/3 lg:w-full mt-2 text-2xl font-bold lg:leading-[81px] text-[#0B1B28] capitalize ">
                 Get your new <span className="text-primary">book</span> with the best price
               </h1>
             </div>
-            <div className=" bg-white rounded-full p-2 shadow-lg">
-              <div class="">
+            <div className=" bg-white rounded-full lg:p-2 shadow-lg">
+              <div class="relative">
                 <form onSubmit={handleSubmit} className="flex justify-between items-center">
                   <input
                     type="text"
                     name="name"
                     placeholder="Search by book name"
-                    class="pl-3"
+                    class="pl-3 w-full bg-white text-sm text-neutral outline-none"
                   />
-                  <span className="bg-primary p-3 rounded-full flex justify-between items-center">
-                    <img src={sicon} alt="" />
-                    <input
-                      type="submit"
-                      value="Search"
-                      class=" text-white pl-2"
-                    />
+                  <span className="bg-primary py-3 px-4 rounded-full flex justify-between items-center">
+                    <img src={sicon} alt="" className="mr-2"/>
+                      <input
+                        type="submit"
+                        value="Search"
+                        class="text-white text-sm mr-2"
+                      />
                   </span>
                 </form>
-                <div className="">
-                  <h3 className="">
-                    {books?.map((book) => (
-                      <p onClick={() => navigateToProductDetail(book._id)} className="text-accent hover:text-primary cursor-pointer py-3">{book.name}</p>
-                    ))}
-                  </h3>
+                <div className="absolute bg-white w-full">
+                      {books?.map((book) => (
+                        <p onClick={() => navigateToProductDetail(book._id)} className="text-accent hover:text-primary text-sm cursor-pointer p-3">{book.name}</p>
+                      ))}
                 </div>
               </div>
             </div>
