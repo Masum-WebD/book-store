869b148446f836cfc2d41dacd8c8bd7c1e3f30a7
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-08-30 23:46:07 +0600

    Added reacr query

--- src/component/AllTopProducts.js
+++ src/component/AllTopProducts.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import SingleProduct from "./SingleProduct";
+import { useQuery } from "react-query";
+import Loading from "./Loading";
 
 const AllTopProducts = () => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
 
-    fetch("https://book-store-46yi.onrender.com/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
+  const { data: products, isLoading } = useQuery("topSellingBooks", () => fetch("https://book-store-46yi.onrender.com/products").then(res => res.json()));
+
+  if (isLoading) {
+    return <Loading />
+  }
 
   return (
     <div className="max-w-[1196px] mx-auto">
@@ -16,7 +17,7 @@ const AllTopProducts = () => {
         Top selling books
       </h2>
       <div className="grid grid-cols-2 lg:grid-cols-5 lg:px-0 px-4 md:container mx-auto my-16 gap-5">
-        {products.slice(12, 17).map((book) => (
+        {products?.slice(12, 17).map((book) => (
           <SingleProduct book={book}></SingleProduct>
         ))}
       </div>
