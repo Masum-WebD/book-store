8eff1795d575c30ecd577ecd938174aa8d072751
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-08-31 00:52:30 +0600

    About us updated

--- src/component/Contact/AboutPage/AboutVission.js
+++ src/component/Contact/AboutPage/AboutVission.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import man from '../../../asset/Images/man1-removebg-preview.png'
+import vision from '../../../asset/Images/Vision statement-pana-min.png'
 const AboutVission = () => {
     return (
-        <div className='conatiner text-left flex justify-around items-center h-[300px] mb-[100px]'>
-            <div className='w-40%'>
-                <img className='bg-[#F8F3FF] w-[280px] h-[350px]' src={man} alt="" />
-            </div>
-            <div className='w-[50%] relative'>
-                <h2 className='text-2xl text-[#0B1B28] capitalize font-bold mb-2'>our Vission</h2>
-                <p className='text-[14px] text-[#0B1B28] mb-8'>Our mission is to provide quality but affordable books for education, entertainment, self-development and self-fulfillment; to all when the need arises by: Providing a wide range of books to satisfy our clients. Exceeding our customers' expectation in their book requirements.</p>
 
-                <div className='absolute top-[60%] right-[5%] h-[20px] w-[20px] bg-yellow-300 rounded-full '></div>
-                <div className='absolute top-[70%] left-[30%] h-[20px] w-[20px] bg-green-300 rounded-full'></div>
-                <div className='absolute top-[10%] left-[50%] h-[20px] w-[20px] bg-pink-300 rounded-full'></div>
+        <div class="hero min-h-screen">
+            <div class="hero-content flex-col lg:flex-row">
+                <img src={vision} class="rounded-lg lg:w-3/6" alt='' />
+                <div>
+                    <h2 className='text-5xl text-neutral text-left capitalize font-bold leading-[70px] mb-2'>Our Vision</h2>
+                    <p className='text-base text-neutral text-left mb-8'>Our mission is to provide quality but affordable books for education, entertainment, self-development and self-fulfillment; to all when the need arises by: Providing a wide range of books to satisfy our clients. Exceeding our customers' expectation in their book requirements.</p>
+                </div>
             </div>
-
         </div>
     );
 };
