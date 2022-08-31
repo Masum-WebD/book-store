f981e7e29d47e9be7845d4d61d2b85284da9ac8b
Ai Shadhin <aishadhin.ac@gmail.com>
2022-08-31 14:26:01 +0600

    redesign

--- src/component/Contact/PopUp/HomePopUp.js
+++ src/component/Contact/PopUp/HomePopUp.js
@@ -21,57 +21,15 @@ const HomePopUp = () => {
                     <label for="my-modal-6" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <div className='flex justify-center items-center'>
                         <div className='popup w-50'>
-                            {/* <div className={state}>
-                                    <div className='arrow'></div>
-                                    <ul className='circle'>
-                                        <li>
-                                            <div className='text' contentEditable='true' spellCheck='false'>$1</div>
-                                        </li>
-                                        <li>
-                                            <div className='text' contentEditable='true' spellCheck='false'>$2</div>
-                                        </li>
-                                        <li>
-                                            <div className='text' contentEditable='true' spellCheck='false'>$3</div>
-                                        </li>
-                                        <li>
-                                            <div className='text' contentEditable='true' spellCheck='false'>$4</div>
-                                        </li>
-                                        <li>
-                                            <div className='text' contentEditable='true' spellCheck='false'>$5</div>
-                                        </li>
-                                        <li>
-                                            <div className='text' contentEditable='true' spellCheck='false'>$6</div>
-                                        </li>
-                                        <li>
-                                            <div className='text' contentEditable='true' spellCheck='false'>$7</div>
-                                        </li>
-                                        <li>
-                                            <div className='text' contentEditable='true' spellCheck='false'>$8</div>
-                                        </li>
-                                        <li>
-                                            <div className='text' contentEditable='true' spellCheck='false'>$9</div>
-                                        </li>
-                                        <li>
-                                            <div className='text' contentEditable='true' spellCheck='false'>$10</div>
-                                        </li>
-                                        <li>
-                                            <div className='text' contentEditable='true' spellCheck='false'>$11</div>
-                                        </li>
-                                        <li>
-                                            <div className='text' contentEditable='true' spellCheck='false'>$12</div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>*/}
 
-                            <div className='w-50 ml-2'>
-                                <h2 className='text-2xl text-lime-900 font-bold mb-2'>New collection Adventure Sale</h2>
-                                <p className='text-black mb-2'>Its the bright one, its the right one,thats offer</p>
-                                <input type="text" placeholder="Type name" class="input input-bordered input-primary w-full max-w-xs mb-2" />
+                            <div className='w-50 pb-4 ml-2'>
+                                <h2 className='text-2xl text-neutral font-bold mb-2'>New collection Adventure Sale</h2>
+                                <p className='text-neutral mb-6'>Its the bright one, its the right one,thats offer</p>
+                                <input type="text" placeholder="Type name" class="input input-primary w-full  mb-2" />
                                 <br />
-                                <input type="email" placeholder="Type email" class="input  input-primary w-full max-w-xs mb-2" />
+                                <input type="email" placeholder="Type email" class="input  input-primary w-full  mb-2" />
                                 <br />
-                                <Link to="/all-products"><button class="spin-btn">Spin</button></Link>
+                                <Link to="/all-products"><button class="text-white w-full bg-primary p-3 rounded-sm">Spin</button></Link>
                             </div>
                         </div>
                     </div>
